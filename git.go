696d941c49dcbcaad466b1ce80035e4d6ae3f209
Yashodhan Ghadge <yashodhanghadge@gmail.com>
2021-08-05 03:19:03 +0530

    wip

--- git.go
+++ git.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "github.com/go-git/go-git/v5"
+    "github.com/go-git/go-git/v5/plumbing/object"
     log "github.com/sirupsen/logrus"
     "os"
 )
@@ -17,12 +18,25 @@ func cloneRepo() {
 }
 
 func ShowLog() {
-    git.
-    _, err := git.PlainClone("/tmp/foo", false, &git.CloneOptions{
-        URL:      "https://github.com/go-git/go-git",
-        Progress: os.Stdout,
-    })
+    r, err := git.PlainOpen(".")
     if err != nil {
         log.Fatal(err)
     }
+
+    // ... retrieving the HEAD reference
+    ref, err := r.Head()
+
+
+    // ... retrieves the commit history
+    cIter, err := r.Log(&git.LogOptions{From: ref.Hash()})
+
+    r.Tags()
+
+    // ... just iterates over the commits
+    var cCount int
+    err = cIter.ForEach(func(c *object.Commit) error {
+        cCount++
+        log.Info(c.String())
+        return nil
+    })
 }
--- git_test.go
+++ git_test.go
@@ -5,3 +5,7 @@ import "testing"
 func Test_cloneRepo(t *testing.T) {
 	cloneRepo()
 }
+
+func TestShowLog(t *testing.T) {
+    ShowLog()
+}
